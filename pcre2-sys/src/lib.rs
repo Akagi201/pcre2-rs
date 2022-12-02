aa3f871b19f49eb57adc07882d8db6c4ded5fdb2
Akagi201 <akagi201@gmail.com>
2022-12-02 12:12:06 +0800

    feat: add lib.rs entrance

--- pcre2-sys/src/lib.rs
+++ pcre2-sys/src/lib.rs
@@ -1,14 +1,3 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
-}
+#![allow(non_camel_case_types)]
+pub mod bindings;
+pub use bindings::*;
