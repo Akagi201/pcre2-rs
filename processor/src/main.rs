8c0ad2c40add512b44afd2581ad8c8ce06fe5847
Akagi201 <akagi201@gmail.com>
2022-12-07 22:08:56 +0800

    feat: init pcre2 pattern match

--- processor/src/main.rs
+++ processor/src/main.rs
@@ -1,5 +1,16 @@
+mod pcre2;
+use pcre2::*;
+
+fn print_matched(target: &str, begin: usize, end: usize) {
+    println!("{}: {}", begin, end);
+    println!("  {}", &target[begin..end]);
+}
+
 fn main() {
-    let target_txt = r"a;jhgoqoghqoj0329 u0tyu10hg0h9Y0Y9827342482y(Y0y(G)_)lajf;lqjfgqhgpqjopjqa=)*(^!@#$%^&*())9999999";
-    let pattern = r"(?<=\D{4})\w{3,11}\w+(?=\w+)";
-    println!("target: {}, pattern: {}", target_txt, pattern);
+    let target = r"a;jhgoqoghqoj0329 u0tyu10hg0h9Y0Y9827342482y(Y0y(G)_)lajf;lqjfgqhgpqjopjqa=)*(^!@#$%^&*())9999999";
+    let pattern = r"(?<=\d{4})[^\d\s]{3,11}(?=.)";
+
+    for mat in Regex::new(pattern).unwrap().find_iter(target) {
+        print_matched(target, mat.0, mat.1);
+    }
 }
